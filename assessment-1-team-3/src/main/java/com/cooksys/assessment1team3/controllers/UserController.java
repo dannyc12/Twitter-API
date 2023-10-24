a62adc2be9e2308e7a4473650e44e02e1960b9de
Danny <94017099+dannyc12@users.noreply.github.com>
2023-10-24 13:41:30 -0500

    Danny (#77)
    * add: added GET request 'getUserByUsername(String username)' in UserController. Created derived method in UserRepository
    
    * fix: added '@' to GET mapping in UserController

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
@@ -1,11 +1,25 @@
 package com.cooksys.assessment1team3.controllers;
 
+import com.cooksys.assessment1team3.dtos.TweetResponseDto;
+import com.cooksys.assessment1team3.dtos.UserResponseDto;
+import com.cooksys.assessment1team3.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/@{username}")
+    public UserResponseDto getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
 }
--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/UserServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/UserServiceImpl.java
@@ -1,5 +1,10 @@
 package com.cooksys.assessment1team3.services.impl;
 
+import com.cooksys.assessment1team3.dtos.UserResponseDto;
+import com.cooksys.assessment1team3.entities.User;
+import com.cooksys.assessment1team3.exceptions.NotFoundException;
+import com.cooksys.assessment1team3.mappers.UserMapper;
+import com.cooksys.assessment1team3.repositories.UserRepository;
 import com.cooksys.assessment1team3.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Override
+    public UserResponseDto getUserByUsername(String username) {
+        User user = userRepository.findByCredentialsUsername(username);
+        if (user == null) {
+            throw new NotFoundException("No user found with username: " + username);
+        }
+        return userMapper.entityToDto(user);
+    }
 }
