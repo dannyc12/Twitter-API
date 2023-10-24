7aae56c58d8ec37da375ed3d01fcb6914ad97a20
dannyc12 <danny.clynes12@gmail.com>
2023-10-24 13:03:57 -0500

    add: added GET request 'getUserByUsername(String username)' in UserController. Created derived method in UserRepository

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
@@ -1,6 +1,10 @@
 package com.cooksys.assessment1team3.controllers;
 
+import com.cooksys.assessment1team3.dtos.UserResponseDto;
+import com.cooksys.assessment1team3.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{username}")
+    public UserResponseDto getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
 }
--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/UserService.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/UserService.java
@@ -1,4 +1,7 @@
 package com.cooksys.assessment1team3.services;
 
+import com.cooksys.assessment1team3.dtos.UserResponseDto;
+
 public interface UserService {
+    UserResponseDto getUserByUsername(String username);
 }
