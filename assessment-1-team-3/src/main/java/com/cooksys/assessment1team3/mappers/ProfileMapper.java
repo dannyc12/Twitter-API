3158183b4c54012e1d4cfd712719780dca83f5ef
dannyc12 <danny.clynes12@gmail.com>
2023-10-24 09:57:56 -0500

    add: added entitiesToDtos method in ProfileMapper

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/mappers/ProfileMapper.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/mappers/ProfileMapper.java
@@ -4,12 +4,16 @@ import com.cooksys.assessment1team3.dtos.ProfileDto;
 import com.cooksys.assessment1team3.entities.Profile;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ProfileMapper {
 
     Profile requestToEntity(ProfileDto profileDto);
 
     ProfileDto entityToDto(Profile entity);
+
+    List<ProfileDto> entitiesToDtos(List<Profile> entities);
 
 
 }
