30135d14bcef07d3400fe110770cae1deb3d7f1e
dannyc12 <danny.clynes12@gmail.com>
2023-10-24 19:58:37 -0500

    add: implemented DELETE request 'deleteUserByUsername' in UserController, UserService and UserServiceImpl

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/controllers/UserController.java
@@ -1,13 +1,10 @@
 package com.cooksys.assessment1team3.controllers;
 
-import com.cooksys.assessment1team3.dtos.TweetResponseDto;
+import com.cooksys.assessment1team3.dtos.CredentialsDto;
 import com.cooksys.assessment1team3.dtos.UserResponseDto;
 import com.cooksys.assessment1team3.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,10 @@ public class UserController {
     @GetMapping("/@{username}")
     public UserResponseDto getUserByUsername(@PathVariable String username) {
         return userService.getUserByUsername(username);
+    }
+
+    @DeleteMapping("/@{username}")
+    public UserResponseDto deleteUserByUsername(@PathVariable String username, @RequestBody CredentialsDto credentialsDto) {
+        return userService.deleteUserByUsername(username, credentialsDto);
     }
 }
