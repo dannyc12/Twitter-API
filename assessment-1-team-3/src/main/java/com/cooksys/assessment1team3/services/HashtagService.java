ec0e2142f33dd26de1135ff40b2e1fae766b1a7f
dannyc12 <danny.clynes12@gmail.com>
2023-10-24 20:25:50 -0500

    add: implemented GET getAllHashtags in HashtagController, Service and ServiceImpl

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/HashtagService.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/HashtagService.java
@@ -1,4 +1,9 @@
 package com.cooksys.assessment1team3.services;
 
+import com.cooksys.assessment1team3.dtos.HashtagDto;
+
+import java.util.List;
+
 public interface HashtagService {
+    List<HashtagDto> getAllHashtags();
 }
--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/HashtagServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/HashtagServiceImpl.java
@@ -1,10 +1,21 @@
 package com.cooksys.assessment1team3.services.impl;
 
+import com.cooksys.assessment1team3.dtos.HashtagDto;
+import com.cooksys.assessment1team3.mappers.HashtagMapper;
+import com.cooksys.assessment1team3.repositories.HashtagRepository;
 import com.cooksys.assessment1team3.services.HashtagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class HashtagServiceImpl implements HashtagService {
+    private final HashtagRepository hashtagRepository;
+    private final HashtagMapper hashtagMapper;
+
+    public List<HashtagDto> getAllHashtags() {
+        return hashtagMapper.entitiesToDtos(hashtagRepository.findAll());
+    }
 }
