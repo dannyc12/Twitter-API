91c3c809b078d82ce0911b2e40e62b5ad1256820
kishan1606 <patelkishan.krp@gmail.com>
2023-10-25 00:01:19 -0500

    Completed Get tags By Label

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/HashtagServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/HashtagServiceImpl.java
@@ -4,9 +4,9 @@ import com.cooksys.assessment1team3.dtos.TweetResponseDto;
 import com.cooksys.assessment1team3.entities.Hashtag;
 import com.cooksys.assessment1team3.entities.Tweet;
 import com.cooksys.assessment1team3.exceptions.NotFoundException;
-import com.cooksys.assessment1team3.mappers.HashtagMapper;
 import com.cooksys.assessment1team3.mappers.TweetMapper;
 import com.cooksys.assessment1team3.repositories.HashtagRepository;
+import com.cooksys.assessment1team3.repositories.TweetRepository;
 import com.cooksys.assessment1team3.services.HashtagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,28 +18,18 @@ import java.util.List;
 public class HashtagServiceImpl implements HashtagService {
 
     private final HashtagRepository hashtagRepository;
+    private final TweetRepository tweetRepository;
     private final TweetMapper tweetMapper;
 
     @Override
     public List<TweetResponseDto> getTagsByLabel(String label) {
-        Hashtag hashtags = hashtagRepository.findByLabel(label);
-        if (hashtags == null) {
-            throw new NotFoundException("No Hashtags found with tags: " + label);
+        String newLabel = "#"+label;
+        Hashtag hashtag = hashtagRepository.findByLabel(newLabel);
+        if (hashtag == null) {
+            throw new NotFoundException("No Hashtags found with tags: " + newLabel);
         }
-        List<Tweet> tweets = hashtags.getTweets();
+        List<Tweet> tweets = tweetRepository.findAllByDeletedFalseAndHashtagsOrderByHashtags(hashtag);
         List<TweetResponseDto> tweetResponseDtos = tweetMapper.entitiesToDtos(tweets);
-        for (int i = 0; i < tweetResponseDtos.size() - 1; i++) {
-            for (int j = i + 1; j < tweetResponseDtos.size(); j++) {
-                TweetResponseDto tweet1 = tweetResponseDtos.get(i);
-                TweetResponseDto tweet2 = tweetResponseDtos.get(j);
-                if (tweet2.getPosted().compareTo(tweet1.getPosted()) > 0) {
-                    // Swap tweet1 and tweet2 if tweet2 is newer than tweet1
-                    TweetResponseDto temp = tweet1;
-                    tweetResponseDtos.set(i, tweet2);
-                    tweetResponseDtos.set(j, temp);
-                }
-            }
-        }
         return tweetResponseDtos;
     }
 }
