429c9c217bfac2e7e916f92c245b18798da6d217
dannyc12 <danny.clynes12@gmail.com>
2023-10-25 14:01:23 -0500

    add: added utils package and utils class. Began moving helper methods into utils class and refactoring UserServiceImpl

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/UserServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/UserServiceImpl.java
@@ -1,16 +1,19 @@
 package com.cooksys.assessment1team3.services.impl;
 
 import com.cooksys.assessment1team3.dtos.*;
+import com.cooksys.assessment1team3.entities.Credentials;
 import com.cooksys.assessment1team3.entities.User;
 import com.cooksys.assessment1team3.exceptions.BadRequestException;
 import com.cooksys.assessment1team3.exceptions.NotAuthorizedException;
 import com.cooksys.assessment1team3.exceptions.NotFoundException;
+import com.cooksys.assessment1team3.mappers.CredentialsMapper;
 import com.cooksys.assessment1team3.mappers.ProfileMapper;
 import com.cooksys.assessment1team3.mappers.TweetMapper;
 import com.cooksys.assessment1team3.mappers.UserMapper;
 import com.cooksys.assessment1team3.repositories.TweetRepository;
 import com.cooksys.assessment1team3.repositories.UserRepository;
 import com.cooksys.assessment1team3.services.UserService;
+import com.cooksys.assessment1team3.utils.Utility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,12 +24,14 @@ import java.util.regex.Pattern;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final Utility utility;
     private final UserRepository userRepository;
     private final TweetRepository tweetRepository;
 
     private final TweetMapper tweetMapper;
     private final UserMapper userMapper;
     private final ProfileMapper profileMapper;
+    private final CredentialsMapper credentialsMapper;
 
     private static final String EMAIL_REGEX =
             "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
@@ -40,19 +45,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponseDto getUserByUsername(String username) {
-        User user = userRepository.findByCredentialsUsername(username);
-        if (user == null || user.isDeleted()) {
-            throw new NotFoundException("No user found with username: " + username);
-        }
+        User user = utility.getUserByUsername(username);
         return userMapper.entityToDto(user);
     }
 
     @Override
     public List<TweetResponseDto> getTweetsByUsername(String username) {
-        User user = userRepository.findByCredentialsUsername(username);
-        if (user == null || user.isDeleted()) {
-            throw new NotFoundException("No user found with username: " + username);
-        }
+        User user = utility.getUserByUsername(username);
         return tweetMapper.entitiesToDtos(tweetRepository.findAllTweetsByAuthorAndDeletedIsFalseOrderByPostedDesc(user));
     }
 
@@ -63,14 +62,9 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponseDto deleteUserByUsername(String username, CredentialsDto credentialsDto) {
-        User user = userRepository.findByCredentialsUsername(username);
-        if (user == null || user.isDeleted()) {
-            throw new NotFoundException("No user found with username: " + username);
-        }
-        if (!user.getCredentials().getUsername().equals(credentialsDto.getUsername())
-                || !user.getCredentials().getPassword().equals(credentialsDto.getPassword())) {
-            throw new NotAuthorizedException("You are not authorized to delete this user.");
-        }
+        User user = utility.getUserByUsername(username);
+        Credentials credentials = credentialsMapper.requestToEntity(credentialsDto);
+        utility.validateCredentials(user, credentials);
         user.setDeleted(true);
         return userMapper.entityToDto(userRepository.saveAndFlush(user));
     }
@@ -78,17 +72,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserResponseDto createUser(UserRequestDto userRequestDto) {
         String username = userRequestDto.getCredentials().getUsername();
-        if (username == null || username.isBlank()) {
-            throw new BadRequestException("Username is required.");
-        }
-        String password = userRequestDto.getCredentials().getPassword();
-        if (password == null || password.isBlank()) {
-            throw new BadRequestException("Password is required.");
-        }
-        String email = userRequestDto.getProfile().getEmail();
-        if (email == null || email.isBlank()) {
-            throw new BadRequestException("Email is required.");
-        }
+        utility.validateUserRequest(userRequestDto);
+        // leaving this because it's a special case
         User user = userRepository.findByCredentialsUsername(username);
         if (user != null) {
             if (user.isDeleted()) {
@@ -104,22 +89,14 @@ public class UserServiceImpl implements UserService {
     }
 
     public UserResponseDto updateUserProfile(String username, UserRequestDto userRequestDto) {
-        // need to extract this portion as a helper method 'getUserByUsername(username)' later
-        User user = userRepository.findByCredentialsUsername(username);
-        if (user == null || user.isDeleted()) {
-            throw new NotFoundException("No user found with username: " + username);
-        }
-        CredentialsDto credentialsDto = userRequestDto.getCredentials();
+        User user = utility.getUserByUsername(username);
+        Credentials credentials = credentialsMapper.requestToEntity(userRequestDto.getCredentials());
         ProfileDto profileDto = userRequestDto.getProfile();
-        // need to extract this portion as a helper method 'validateUserCredentials(user, credentialsDto)' later
-        if (!user.getCredentials().getUsername().equals(credentialsDto.getUsername())
-                || !user.getCredentials().getPassword().equals(credentialsDto.getPassword())) {
-            throw new NotAuthorizedException("You are not authorized to update this user.");
-        }
+        utility.validateCredentials(user, credentials);
         if (profileDto.getEmail() == null) {
             throw new BadRequestException("An email is required to update user profile.");
         }
-        if (!validateUserEmail(profileDto.getEmail())) {
+        if (!utility.validateUserEmail(profileDto.getEmail())) {
             throw new BadRequestException("You must pass a valid email to update the user profile.");
         }
         user.setProfile(profileMapper.requestToEntity(profileDto));
---
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/utils/Utility.java
@@ -0,0 +1,59 @@
+package com.cooksys.assessment1team3.utils;
+
+import com.cooksys.assessment1team3.dtos.UserRequestDto;
+import com.cooksys.assessment1team3.entities.Credentials;
+import com.cooksys.assessment1team3.entities.User;
+import com.cooksys.assessment1team3.exceptions.BadRequestException;
+import com.cooksys.assessment1team3.exceptions.NotAuthorizedException;
+import com.cooksys.assessment1team3.exceptions.NotFoundException;
+import com.cooksys.assessment1team3.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@RequiredArgsConstructor
+public class Utility {
+    private final UserRepository userRepository;
+    private static final String EMAIL_REGEX =
+            "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+
+    private static final Pattern pattern = Pattern.compile(EMAIL_REGEX);
+
+    public boolean validateUserEmail(String email) {
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public User getUserByUsername(String username) {
+        User user = userRepository.findByCredentialsUsername(username);
+        if (user == null || user.isDeleted()) {
+            throw new NotFoundException("No user found with username: " + username);
+        }
+        return user;
+    }
+
+    public void validateCredentials(User user, Credentials credentials) {
+        if (!user.getCredentials().getUsername().equals(credentials.getUsername())
+                || !user.getCredentials().getPassword().equals(credentials.getPassword())) {
+            // we could pass in a specific string here for each case, but we'd need another parameter
+            throw new NotAuthorizedException("You are not authorized to perform this action.");
+        }
+    }
+
+    public void validateUserRequest(UserRequestDto userRequestDto) {
+        String username = userRequestDto.getCredentials().getUsername();
+        if (username == null || username.isBlank()) {
+            throw new BadRequestException("Username is required.");
+        }
+        String password = userRequestDto.getCredentials().getPassword();
+        if (password == null || password.isBlank()) {
+            throw new BadRequestException("Password is required.");
+        }
+        String email = userRequestDto.getProfile().getEmail();
+        if (email == null || email.isBlank()) {
+            throw new BadRequestException("Email is required.");
+        }
+    }
+
+}
