6487ce98ba1fe7fbbf55e588f7abea29018c64d4
dannyc12 <danny.clynes12@gmail.com>
2023-10-26 14:17:23 -0500

    optimized imports

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/TweetServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/TweetServiceImpl.java
@@ -1,19 +1,12 @@
 package com.cooksys.assessment1team3.services.impl;
 
-import com.cooksys.assessment1team3.dtos.ContextDto;
-import com.cooksys.assessment1team3.dtos.CredentialsDto;
-import com.cooksys.assessment1team3.dtos.HashtagDto;
-import com.cooksys.assessment1team3.dtos.TweetResponseDto;
-import com.cooksys.assessment1team3.dtos.UserResponseDto;
-
+import com.cooksys.assessment1team3.dtos.*;
 import com.cooksys.assessment1team3.entities.Tweet;
 import com.cooksys.assessment1team3.entities.User;
-
 import com.cooksys.assessment1team3.mappers.CredentialsMapper;
 import com.cooksys.assessment1team3.mappers.HashtagMapper;
 import com.cooksys.assessment1team3.mappers.TweetMapper;
 import com.cooksys.assessment1team3.mappers.UserMapper;
-
 import com.cooksys.assessment1team3.repositories.TweetRepository;
 import com.cooksys.assessment1team3.repositories.UserRepository;
 import com.cooksys.assessment1team3.services.TweetService;
@@ -21,7 +14,10 @@ import com.cooksys.assessment1team3.utils.Utility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
