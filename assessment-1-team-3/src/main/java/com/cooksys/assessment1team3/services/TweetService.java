8b07c3562102aa56547a6b7c464db99a2154cf16
dannyc12 <danny.clynes12@gmail.com>
2023-10-25 19:42:09 -0500

    continued working on POST repostTweet in TweetController and ServiceImpl

--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/TweetService.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/TweetService.java
@@ -1,5 +1,6 @@
 package com.cooksys.assessment1team3.services;
 
+import com.cooksys.assessment1team3.dtos.CredentialsDto;
 import com.cooksys.assessment1team3.dtos.TweetResponseDto;
 
 import java.util.List;
@@ -8,4 +9,6 @@ public interface TweetService {
     List<TweetResponseDto> getAllTweets();
 
     List<TweetResponseDto> getTweetRepliesById(Long id);
+
+    TweetResponseDto repostTweet(Long id, CredentialsDto credentialsDto);
 }
--- assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/TweetServiceImpl.java
+++ assessment-1-team-3/src/main/java/com/cooksys/assessment1team3/services/impl/TweetServiceImpl.java
@@ -1,11 +1,18 @@
 package com.cooksys.assessment1team3.services.impl;
 
+import com.cooksys.assessment1team3.dtos.CredentialsDto;
 import com.cooksys.assessment1team3.dtos.TweetResponseDto;
+import com.cooksys.assessment1team3.entities.Credentials;
 import com.cooksys.assessment1team3.entities.Tweet;
+import com.cooksys.assessment1team3.entities.User;
+import com.cooksys.assessment1team3.exceptions.NotAuthorizedException;
 import com.cooksys.assessment1team3.exceptions.NotFoundException;
+import com.cooksys.assessment1team3.mappers.CredentialsMapper;
 import com.cooksys.assessment1team3.mappers.TweetMapper;
 import com.cooksys.assessment1team3.repositories.TweetRepository;
+import com.cooksys.assessment1team3.repositories.UserRepository;
 import com.cooksys.assessment1team3.services.TweetService;
+import com.cooksys.assessment1team3.utils.Utility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +24,9 @@ import java.util.Optional;
 public class TweetServiceImpl implements TweetService {
     private final TweetRepository tweetRepository;
     private final TweetMapper tweetMapper;
+    private final CredentialsMapper credentialsMapper;
+    private final UserRepository userRepository;
+    private final Utility utility;
 
     @Override
     public List<TweetResponseDto> getAllTweets() {
@@ -29,5 +39,29 @@ public class TweetServiceImpl implements TweetService {
             throw new NotFoundException("No tweet found with id: " + id);
         }
         return tweetMapper.entitiesToDtos(tweetRepository.findAllRepliesToTweet(id));
+    }
+
+    @Override
+    public TweetResponseDto repostTweet(Long id, CredentialsDto credentialsDto) {
+        Optional<Tweet> optionalTweet = tweetRepository.findById(id);
+        if (optionalTweet.isEmpty() || optionalTweet.get().isDeleted()) {
+            throw new NotFoundException("No tweet found with id: " + id);
+        }
+        System.out.println(credentialsDto.getUsername());
+        String username = credentialsDto.getUsername();
+//        Credentials credentials = credentialsMapper.requestToEntity(credentialsDto);
+        // validate user exists
+        User author = userRepository.findByCredentialsUsername(username);
+        utility.validateUserExists(author, username);
+        if (!author.getCredentials().getUsername().equals(credentialsDto.getUsername())
+                || !author.getCredentials().getPassword().equals(credentialsDto.getPassword())
+        ) {
+            throw new NotAuthorizedException("You are not authorized to repost this tweet.");
+        }
+        Tweet tweet = optionalTweet.get();
+        tweet.setRepostOf(optionalTweet.get());
+        tweet.setContent(null);
+        tweet.setAuthor(author);
+        return tweetMapper.entityToDto(tweetRepository.saveAndFlush(tweet));
     }
 }
